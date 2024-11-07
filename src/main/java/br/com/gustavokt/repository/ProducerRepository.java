5601597b71224bb505a988f7fc78d513061257dd
Gustavo Cronemberger <gustavocronemberger@gmail.com>
2024-11-07 19:15:31 -0300

    service ok

--- src/main/java/br/com/gustavokt/repository/ProducerRepository.java
+++ src/main/java/br/com/gustavokt/repository/ProducerRepository.java
@@ -50,7 +50,7 @@ public class ProducerRepository {
     }
 
     private static PreparedStatement createPreparedStatementDelete(Connection conn, Integer id) throws SQLException {
-        String sql = "DELETE FROM `farm_catalog`.`producer` WHERE (`id` = ?);";
+        String sql = "DELETE FROM farm_catalog.producer WHERE (id = ?);";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setInt(1, id);
         return ps;
@@ -67,7 +67,7 @@ public class ProducerRepository {
     }
 
     private static PreparedStatement createPreparedStatementSave (Connection conn, Producer producer) throws SQLException {
-        String sql = "INSERT INTO `farm_catalog`.`producer` (`name`) VALUES (?);";
+        String sql = "INSERT INTO farm_catalog.producer (name) VALUES (?);";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setString(1, producer.getName());
         return ps;
@@ -105,7 +105,7 @@ public class ProducerRepository {
     }
 
     private static PreparedStatement createPreparedStatementUpdate (Connection conn, Producer producer) throws SQLException {
-        String sql = "UPDATE `farm_catalog`.`producer` SET `name` = ? WHERE (`id` = ?);";
+        String sql = "UPDATE farm_catalog.producer SET name = ? WHERE (id = ?);";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setString(1, producer.getName());
         ps.setInt(2, producer.getId());
