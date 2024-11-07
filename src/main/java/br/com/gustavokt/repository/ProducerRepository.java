0322fbb0d626bdfbd59b06e220fe90ec4274642a
Gustavo Cronemberger <gustavocronemberger@gmail.com>
2024-11-06 21:46:22 -0300

    final repository

--- src/main/java/br/com/gustavokt/repository/ProducerRepository.java
+++ src/main/java/br/com/gustavokt/repository/ProducerRepository.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Log4j2
@@ -29,5 +30,85 @@ public class ProducerRepository {
             log.error("Error while not founding, trying to insert", e);
         }
         return producers;
+    }
+
+    private static PreparedStatement createPreparedStatementFindByName(Connection conn, String name) throws SQLException {
+        String sql = "SELECT * FROM fazenda_catalog.producer where name like ?;";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, String.format("%%%s%%", name));
+        return ps;
+    }
+
+    public static void delete(int id) {
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPreparedStatementDelete(conn, id)) {
+            ps.execute();
+            log.info("Deleted producer '{}' from the database", id);
+        } catch (SQLException e) {
+            log.error("Error while trying to delete producer '{}'", id, e);
+        }
+    }
+
+    private static PreparedStatement createPreparedStatementDelete(Connection conn, Integer id) throws SQLException {
+        String sql = "DELETE FROM `fazenda_catalog`.`producer` WHERE (`id` = ?);";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, id);
+        return ps;
+    }
+
+    public static void save (Producer producer){
+        log.info("Saving producer '{}'", producer);
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPreparedStatementSave(conn, producer)) {
+            ps.execute();
+        }catch (SQLException e){
+            log.error("Error while trying to save producer '{}'", producer.getId(), e);
+        }
+    }
+
+    private static PreparedStatement createPreparedStatementSave (Connection conn, Producer producer) throws SQLException {
+        String sql = "INSERT INTO `fazenda_catalog`.`producer` (`name`) VALUES (?);";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, producer.getName());
+        return ps;
+    }
+
+    public static Optional<Producer> findById(Integer id) {
+        log.info("Finding Producers by Id '{}'", id);
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPreparedStatementFindById(conn, id);
+             ResultSet rs = ps.executeQuery()) {
+            if (!rs.next()) return Optional.empty();
+            return Optional.of(Producer.builder().id(rs.getInt("id")).name(rs.getString("name")).build());
+        } catch (SQLException e) {
+            log.error("Error while not founding, trying to find all producers", e);
+        }
+        return Optional.empty();
+    }
+
+    private static PreparedStatement createPreparedStatementFindById (Connection conn, Integer id) throws SQLException {
+        String sql = "SELECT * FROM fazenda_catalog.producer where id = ?;";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, id);
+        return ps;
+    }
+
+    public static void update (Producer producer){
+        log.info("Updating producer '{}'", producer);
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPreparedStatementUpdate(conn, producer)) {
+            ps.execute();
+            log.info("Updated producer '{}'", producer.getId());
+        }catch (SQLException e){
+            log.error("Error while trying to update producer '{}'", producer.getId(), e);
+        }
+    }
+
+    private static PreparedStatement createPreparedStatementUpdate (Connection conn, Producer producer) throws SQLException {
+        String sql = "UPDATE `fazenda_catalog`.`producer` SET `name` = ? WHERE (`id` = ?);";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, producer.getName());
+        ps.setInt(2, producer.getId());
+        return ps;
     }
 }
