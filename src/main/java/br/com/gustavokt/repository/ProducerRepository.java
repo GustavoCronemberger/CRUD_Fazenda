508dd4d2a7af1546a2607bdb69a52b07acf7c9d4
Gustavo Cronemberger <gustavocronemberger@gmail.com>
2024-11-06 21:35:56 -0300

    repository 3

--- src/main/java/br/com/gustavokt/repository/ProducerRepository.java
+++ src/main/java/br/com/gustavokt/repository/ProducerRepository.java
@@ -1,7 +1,33 @@
 package br.com.gustavokt.repository;
 
+import br.com.gustavokt.conn.ConnectionFactory;
+import br.com.gustavokt.domain.Producer;
 import lombok.extern.log4j.Log4j2;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Log4j2
 public class ProducerRepository {
+    public static List<Producer> findByName(String name) {
+        log.info("Finding Producers by Name '{}'", name);
+        List<Producer> producers = new ArrayList<>();
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPreparedStatementFindByName(conn, name);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Producer producer = Producer.builder().id(rs.getInt("id")).name(rs.getString("name")).build();
+                producers.add(producer);
+            }
+
+        } catch (SQLException e) {
+            log.error("Error while not founding, trying to insert", e);
+        }
+        return producers;
+    }
 }
